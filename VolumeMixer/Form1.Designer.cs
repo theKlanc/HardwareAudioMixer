26c30647fc0584dbd8739f565bff0252d555c305
Klanc <clank201@gmail.com>
2017-09-23 21:44:50 +0200

    Add project files.

---
+++ VolumeMixer/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace VolumeMixer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(26, 122);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(152, 122);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(88, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ VolumeMixer/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using AudioSwitcher.AudioApi.CoreAudio;
+using AudioSwitcher.AudioApi.Session;
+
+namespace VolumeMixer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            port1 = new SerialPort("COM8", 9600);
+            port1.Open();
+            defaultPlaybackDevice = new CoreAudioController().DefaultPlaybackDevice;
+            label1.Text = defaultPlaybackDevice.SessionController.ToArray()[actual].DisplayName;
+            progressBar1.Value = (int)defaultPlaybackDevice.SessionController.ToArray()[actual].Volume;
+            try
+            {
+                pictureBox1.Image = Icon.ExtractAssociatedIcon(defaultPlaybackDevice.SessionController.ToArray()[actual].ExecutablePath).ToBitmap();
+                pictureBox1.Load();
+            }
+            catch { }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string entradaS = port1.ReadLine();
+            port1.DiscardOutBuffer();
+            port1.DiscardInBuffer();
+            int entrada = 0;
+            bool shaEntrat = false;
+            try
+            {
+                entrada = Int32.Parse(entradaS);
+                shaEntrat = true;
+            }
+            catch { }
+            if (shaEntrat)
+            {
+                bool upButton = false, downButton = false, switchButton = false;
+                if (entrada >= 4) { downButton = true; entrada -= 4; }
+                if (entrada >= 2) { upButton = true; entrada -= 2; }
+                if (entrada >= 1) { switchButton = true; entrada--; }
+                if (switchButton && !lastSwitch)
+                {
+                    actual++;
+                    if (actual >= defaultPlaybackDevice.SessionController.ToArray().Length) actual = 0;
+                    try
+                    {
+                        label1.Text = defaultPlaybackDevice.SessionController.ToArray()[actual].DisplayName;
+                        progressBar1.Value = (int)defaultPlaybackDevice.SessionController.ToArray()[actual].Volume;
+                        pictureBox1.Image = Icon.ExtractAssociatedIcon(defaultPlaybackDevice.SessionController.ToArray()[actual].ExecutablePath).ToBitmap();
+                        pictureBox1.Update();
+                    }
+                    catch { }
+                }
+                if (upButton && progressBar1.Value < 100) { defaultPlaybackDevice.SessionController.ToArray()[actual].Volume++; progressBar1.Value = (int)defaultPlaybackDevice.SessionController.ToArray()[actual].Volume; }
+                if (downButton && progressBar1.Value > 0) { defaultPlaybackDevice.SessionController.ToArray()[actual].Volume--; progressBar1.Value = (int)defaultPlaybackDevice.SessionController.ToArray()[actual].Volume; }
+                lastSwitch = switchButton;
+            }
+        }
+        private SerialPort port1;
+        private CoreAudioDevice defaultPlaybackDevice;
+        private int actual = 0;
+        private bool lastSwitch = false;
+    }
+}
